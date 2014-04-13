33c5d3cad6ea0daa3b3a7030311824e62816517f
chetanburande <chetan.burande7@gmail.com>
2014-04-12 20:20:37 -0700

    changes to handle Ajax call

--- library/src/main/resources/assets/js/app.js
+++ library/src/main/resources/assets/js/app.js
@@ -1,4 +1,15 @@
 $(":button").click(function() {
 	var isbn = this.id;
 	alert('About to report lost on ISBN ' + isbn);
+	$.ajax({        
+        url: 'v1/books/'+isbn+'?status=lost',
+        type: 'PUT'      
+        //
+    });
+    
+   
+    this.disabled=true;
+   
+    $('#bookStatus_'+isbn).html("lost");
+	
 });
